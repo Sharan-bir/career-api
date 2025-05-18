d9042c9ffd52f0407faa9ffeb6988d622dc138a0
Sharanbir <sharanbir.3000@gmail.com>
2025-05-18 19:19:43 +0530

    first commit3

--- job_portal/settings.py
+++ job_portal/settings.py
@@ -141,3 +141,4 @@ CORS_ALLOW_ALL_ORIGINS = True  # For development only, configure properly for pr
 # Media files configuration
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = '/staticfiles/'
